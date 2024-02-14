459f1a34e2d4b7e1538b7a1e9ace0c5a2cfea565
minbee <alswl4223@naver.com>
2024-02-14 16:37:01 +0900

    feat : 주석으로 구현 틀 짜기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import java.io.*;
+import java.util.*;
+import camp.nextstep.edu.missionutils.*;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        // TODO: 숫자 야구 게임 구현
+        // 서로 다른 3개의 수를 랜덤으로 뽑는다. -> LinkedList로 구현
+        // 3개의 수를 입력받는다. -> 하나하나 빼서 LinkedList에 넣는다.
+        // 같은 수가 같은 자리에 있으면 스트라이크, 같은 수가 다른 자리에 있으면 볼
+        // 3개의 숫자를 모두 맞히면 종료
+        // 잘못된 값을 입력받으면 IllegalArgumentException 출력
+
     }
 }
