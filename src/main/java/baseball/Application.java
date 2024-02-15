6e73064ac652da7fb0bf984571520f3d44e3bc1b
minbee <alswl4223@naver.com>
2024-02-15 15:27:24 +0900

    fix : 게임을 지속하기 위한 while문 추가 및 인덴트를 위한 메소드 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,16 +6,11 @@ import camp.nextstep.edu.missionutils.*;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 숫자 야구 게임 구현
-        /* 서로 다른 3개의 수를 랜덤으로 뽑는다. -> LinkedList로 구현 */
 
-        // 랜덤으로 생성한 수를 넣는 LinkedList
-        LinkedList<Integer> randomNum = new LinkedList<>();
+    static LinkedList<Integer> randomNum;
+    static LinkedList<Integer> userNum;
 
-        // 사용자에게 입력받은 수를 넣는 LinkedList
-        LinkedList<Integer> userNum = new LinkedList<>();
-
+    static void pickNum() {
         // 수 중복되지 않게 3가지 뽑기
         while (randomNum.size() != 3) {
             int ranNum = Randoms.pickNumberInRange(1, 9);
@@ -23,26 +18,49 @@ public class Application {
                 randomNum.add(ranNum);
             }
         }
+    }
 
-        // user에게 수 입력받기
-        System.out.printf("숫자를 입력해주세요");
-        String[] userArr = Console.readLine().split("");
-
-        // 사용자가 수를 잘못 입력했을 때
+    static void checkUserNum(String[] userArr) {
         if (userArr.length != 3) {
             throw new IllegalArgumentException();
         }
 
-        // 입력받은 수를 LinkedList에 넣기
+    }
 
-        userNum.add(Integer.parseInt(userArr[0]));
-        userNum.add(Integer.parseInt(userArr[1]));
-        userNum.add(Integer.parseInt(userArr[2]));
+    public static void main(String[] args) {
+        // TODO: 숫자 야구 게임 구현
+        while (true) {
+            /* 서로 다른 3개의 수를 랜덤으로 뽑는다. -> LinkedList로 구현 */
 
-        // 같은 수가 같은 자리에 있으면 스트라이크, 같은 수가 다른 자리에 있으면 볼
+            // 랜덤으로 생성한 수를 넣는 LinkedList
+            randomNum = new LinkedList<>();
+            pickNum();
 
-        // 3개의 숫자를 모두 맞히면 종료
-        // 잘못된 값을 입력받으면 IllegalArgumentException 출력
+            while (true) {
+
+                // 사용자에게 입력받은 수를 넣는 LinkedList
+                userNum = new LinkedList<>();
+
+                // user에게 수 입력받기
+                System.out.printf("숫자를 입력해주세요");
+                String[] userArr = Console.readLine().split("");
+
+                // 사용자가 수를 잘못 입력했는지 확인
+                checkUserNum(userArr);
+
+                // 입력받은 수를 LinkedList에 넣기
+                userNum.add(Integer.parseInt(userArr[0]));
+                userNum.add(Integer.parseInt(userArr[1]));
+                userNum.add(Integer.parseInt(userArr[2]));
+
+                // 같은 수가 같은 자리에 있으면 스트라이크, 같은 수가 다른 자리에 있으면 볼
+
+                // 3개의 숫자를 모두 맞히면 종료
+                // 잘못된 값을 입력받으면 IllegalArgumentException 출력
+
+            }
+
+        }
 
     }
 }
