c0a578a7f4b32250bfa73c80ad35609723277be9
minbee <alswl4223@naver.com>
2024-02-15 16:04:48 +0900

    refactor : 출력 기능 메서드로 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -56,6 +56,27 @@ public class Application {
         return 0;
     }
 
+    // 값 print하기
+    static void printAnswer(int strike, int ball) {
+        // strike와 ball이 섞임
+        if (strike != 0 && ball != 0) {
+            System.out.printf("%d볼 %스트라이크\n", ball, strike);
+        }
+        // strike만
+        else if (strike != 0 && ball == 0) {
+            System.out.printf("%스트라이크\n", strike);
+        }
+        // ball만
+        else if (strike != 0 && ball != 0) {
+            System.out.printf("%d볼\n", ball);
+        }
+        // 둘 다 0일 때
+        else {
+            System.out.printf("낫싱\n");
+        }
+
+    }
+
     // 메인
     public static void main(String[] args) {
         // TODO: 숫자 야구 게임 구현
@@ -92,23 +113,8 @@ public class Application {
                 // 3개의 숫자를 모두 맞히면 종료
                 if (strike == 3) {
                     break;
-                }
-                // strike와 ball이 섞임
-                else if (strike != 0 && ball != 0) {
-                    System.out.printf("%d볼 %스트라이크\n", ball, strike);
-                }
-                // strike만
-                else if (strike != 0 && ball == 0) {
-                    System.out.printf("%스트라이크\n", strike);
-                }
-                // ball만
-                else if (strike != 0 && ball != 0) {
-                    System.out.printf("%d볼\n", ball);
-                }
-                // 둘 다 0일 때
-                else {
-                    System.out.printf("낫싱\n");
-                }
+                } else
+                    printAnswer(strike, ball);
 
             }
 
