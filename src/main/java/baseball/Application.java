4fb6534e430b513f53ae785770560173fc11b9e2
minbee <alswl4223@naver.com>
2024-02-15 16:20:44 +0900

    fix : main에서 결과 출력하는 부분 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -60,14 +60,14 @@ public class Application {
     static void printAnswer(int strike, int ball) {
         // strike와 ball이 섞임
         if (strike != 0 && ball != 0) {
-            System.out.printf("%d볼 %스트라이크\n", ball, strike);
+            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
         }
         // strike만
         else if (strike != 0 && ball == 0) {
-            System.out.printf("%스트라이크\n", strike);
+            System.out.printf("%d스트라이크\n", strike);
         }
         // ball만
-        else if (strike != 0 && ball != 0) {
+        else if (strike == 0 && ball != 0) {
             System.out.printf("%d볼\n", ball);
         }
         // 둘 다 0일 때
@@ -113,8 +113,9 @@ public class Application {
                 // 3개의 숫자를 모두 맞히면 종료
                 if (strike == 3) {
                     break;
-                } else
+                } else {
                     printAnswer(strike, ball);
+                }
 
             }
 
