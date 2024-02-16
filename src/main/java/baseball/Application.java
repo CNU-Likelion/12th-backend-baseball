c147daac38aace7f5599e2e4de68ccf3ea8d8311
Shsin9797 <aaa8141@naver.com>
2024-02-17 00:25:56 +0900

    feat: check if input num is correct or not

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
+import static mallang.missionutils.Console.*;
 
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+    }
+
+    //메서드 선언
+
+    public static int checkInputNum() throws IllegalArgumentException{
+        int k ;
+        k= Integer.parseInt( readLine() );
+        if (!(( 1 <= k) & ( k <= 9 ))){
+            throw new IllegalArgumentException();
+        }
+        return k;
     }
 }
