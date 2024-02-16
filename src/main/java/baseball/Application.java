122f690c2f8baaa9a5e396804009a66cd7c7ca9d
CHAEGODA <chae_goda@naver.com>
2024-02-17 08:26:36 +0900

    feat : Application1
    1 ~ 9 서로 다른 임의의 수 생성 후 배열에 저장

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
+import java.util.*;
+import mallang.missionutils.Randoms;
+import mallang.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
     }
+    // 1부터 9까지의 서로 다른 임의의 수를 3개 생성해서 배열에 저장
+    public int[] answerNum() {
+        int[] answer = new int[3];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = Randoms.pickNumberInRange(1,9);
+        }
+        return answer;
+    }
+
 }
