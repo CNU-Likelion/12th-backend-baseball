113be26d9e507fc0a4bf48fcd54aba5df271784d
Shsin9797 <aaa8141@naver.com>
2024-02-17 23:14:33 +0900

    feat : printResult

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -74,4 +74,17 @@ public class Application {
 
         return new int[] {strike,ball};
     }
+
+    public static void printResult(int strike, int ball){
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            checkGameRestart();
+        } else if ((strike != 0) & (ball != 0)) {
+            System.out.printf("%d볼 %d스트라이크\n",strike,ball);
+        } else if (strike != 0) {
+            System.out.printf("%d스트라이크\n",strike);
+        } else if (ball != 0) {
+            System.out.printf("%d볼\n", ball);
+        }
+    }
 }
