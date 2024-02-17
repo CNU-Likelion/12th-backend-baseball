e3ca71aee61e1f7fd8d4322bfe547f0953199d9b
Shsin9797 <aaa8141@naver.com>
2024-02-17 23:27:48 +0900

    fix : wantGameRestart

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -75,16 +75,21 @@ public class Application {
         return new int[] {strike,ball};
     }
 
-    public static void printResult(int strike, int ball){
+    public static boolean printResult(int strike, int ball){
         if (strike == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            checkGameRestart();
+            return wantGameRestart();
         } else if ((strike != 0) & (ball != 0)) {
             System.out.printf("%d볼 %d스트라이크\n",strike,ball);
+            return true;
         } else if (strike != 0) {
             System.out.printf("%d스트라이크\n",strike);
+            return true;
         } else if (ball != 0) {
             System.out.printf("%d볼\n", ball);
+            return true;
+        } else {
+            return true;
         }
     }
 
