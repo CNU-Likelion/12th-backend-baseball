cb39f03c3b06c49320043ca1efdfdc421afbe26b
minbee <alswl4223@naver.com>
2024-02-18 17:30:23 +0900

    refactor : 클래스로 메서드 기능별로 묶기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,49 +2,9 @@ package baseball;
 
 import java.util.*;
 import mallang.missionutils.*;
-import mallang.missionutils.Console;
 
-public class Application {
-
-    static void pickNum(List<Integer> randomNum) {
-        while (randomNum.size() != 3) {
-            int ranNum = Randoms.pickNumberInRange(1, 9);
-            if (!randomNum.contains(ranNum)) {
-                randomNum.add(ranNum);
-            }
-        }
-    }
-
-    static String[] getUserNum() {
-        System.out.printf("숫자를 입력해주세요");
-        return Console.readLine().split("");
-    }
-
-    static void checkUserNum(String[] userArr) {
-        if (userArr.length != 3) {
-            throw new IllegalArgumentException();
-        }
-
-    }
-
-    static List<Integer> addNumToList(List<Integer> userNum, String[] userArr) {
-        userNum.add(Integer.parseInt(userArr[0]));
-        userNum.add(Integer.parseInt(userArr[1]));
-        userNum.add(Integer.parseInt(userArr[2]));
-        return userNum;
-    }
-
-    static int countStrike(List<Integer> randomNum, List<Integer> userNum) {
-        int cnt = 0;
-        for (int i = 0; i < 3; i++) {
-            if (randomNum.get(i) == userNum.get(i)) {
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-
-    static int countBall(List<Integer> userNum, List<Integer> randomNum) {
+class Count {
+    int countBall(List<Integer> userNum, List<Integer> randomNum) {
         int cnt = 0;
         for (int i = 0; i < 3; i++) {
             cnt += checkBall(i, userNum, randomNum);
@@ -52,7 +12,7 @@ public class Application {
         return cnt;
     }
 
-    static int checkBall(int index, List<Integer> userNum, List<Integer> randomNum) {
+    int checkBall(int index, List<Integer> userNum, List<Integer> randomNum) {
         if (randomNum.contains(userNum.get(index))) {
             if (index != randomNum.indexOf(userNum.get(index))) {
                 return 1;
@@ -61,7 +21,50 @@ public class Application {
         return 0;
     }
 
-    static int printAnswer(int strike, int ball) {
+    int countStrike(List<Integer> randomNum, List<Integer> userNum) {
+        int cnt = 0;
+        for (int i = 0; i < 3; i++) {
+            if (randomNum.get(i) == userNum.get(i)) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
+
+class HandleUserNum {
+    String[] getUserNum() {
+        System.out.printf("숫자를 입력해주세요");
+        return Console.readLine().split("");
+    }
+
+    void checkUserNum(String[] userArr) {
+        if (userArr.length != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    List<Integer> addNumToList(List<Integer> userNum, String[] userArr) {
+        userNum.add(Integer.parseInt(userArr[0]));
+        userNum.add(Integer.parseInt(userArr[1]));
+        userNum.add(Integer.parseInt(userArr[2]));
+        return userNum;
+    }
+}
+
+class HandleRandomNum {
+    void pickNum(List<Integer> randomNum) {
+        while (randomNum.size() != 3) {
+            int ranNum = Randoms.pickNumberInRange(1, 9);
+            if (!randomNum.contains(ranNum)) {
+                randomNum.add(ranNum);
+            }
+        }
+    }
+}
+
+class HandleProgress {
+    int printAnswer(int strike, int ball) {
         if (strike == 3) {
             System.out.println("3스트라이크");
             return 0;
@@ -77,13 +80,13 @@ public class Application {
         return 1;
     }
 
-    static int askFinish() {
+    int askFinish() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Integer.parseInt(Console.readLine());
     }
 
-    static int endOrAgain(int again) {
+    int endOrAgain(int again) {
         if (again == 1) {
             return 1;
         } else if (again == 2) {
@@ -94,7 +97,15 @@ public class Application {
         }
     }
 
+}
+
+public class Application {
     public static void main(String[] args) {
+
+        Count count = new Count();
+        HandleUserNum handleUserNum = new HandleUserNum();
+        HandleRandomNum handleRandomNum = new HandleRandomNum();
+        HandleProgress handleProgress = new HandleProgress();
 
         List<Integer> randomNum;
         List<Integer> userNum;
@@ -104,7 +115,7 @@ public class Application {
 
         while (doGameSet == 1) {
             randomNum = new ArrayList<>();
-            pickNum(randomNum);
+            handleRandomNum.pickNum(randomNum);
 
             while (doUserSet == 1) {
                 int strike = 0;
@@ -112,19 +123,19 @@ public class Application {
 
                 userNum = new ArrayList<>();
 
-                String[] userArr = getUserNum();
+                String[] userArr = handleUserNum.getUserNum();
 
-                checkUserNum(userArr);
-                addNumToList(userNum, userArr);
+                handleUserNum.checkUserNum(userArr);
+                handleUserNum.addNumToList(userNum, userArr);
 
-                strike = countStrike(randomNum, userNum);
-                ball = countBall(userNum, randomNum);
+                strike = count.countStrike(randomNum, userNum);
+                ball = count.countBall(userNum, randomNum);
 
-                doUserSet = printAnswer(strike, ball);
+                doUserSet = handleProgress.printAnswer(strike, ball);
             }
 
-            int again = askFinish();
-            doGameSet = endOrAgain(again);
+            int again = handleProgress.askFinish();
+            doGameSet = handleProgress.endOrAgain(again);
             doUserSet = 1;
 
         }
