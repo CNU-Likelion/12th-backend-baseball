b83c73c88bec525fb3250b128335612b14bb3432
Shsin9797 <aaa8141@naver.com>
2024-02-19 21:47:56 +0900

    refactor : fit method convention & remove comment

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,6 +8,10 @@ public class Application {
         startBallGame();
     }
 
+    Application(){
+
+    }
+
     public static void startBallGame(){
         boolean doGame = true;
 
@@ -27,23 +31,16 @@ public class Application {
     }
 
     //메서드 선언
+    public static int[] setComputerNums() {
+        int[] computerNums = new int[3];
 
-    public static boolean checkInputNum(String a,int start,int end) throws IllegalArgumentException{
-
-        try {
-            int b = Integer.parseInt(a) ;
-            if ((start <= b) && (b <=end)) {
-                return true;
-            } else {
-                throw new IllegalArgumentException();
-            }
-        } catch (IllegalArgumentException e){
-            throw e;
+        for (int i = 0 ; i < 3 ; i++) {
+            computerNums[i] = pickNumberInRange(1,9);
         }
 
+        return computerNums;
     }
 
-    //이용자로부터 숫자 3개를 받아 저장하는 함수
     public static int[] getThreeNums() {
         int[] userNums = new int[3];
         String k ;
@@ -59,56 +56,22 @@ public class Application {
         return userNums;
     }
 
-    //숫자로 분리하기
-    public static int[] detachNum(String k) {
-        int[] numsList = new int[3];
-        for (int i = 0; i < 3; i++) {
-            char b = k.charAt(i);
-            String a = String.valueOf(b);
-            if (checkInputNum(a, 1, 9)) {
-                numsList[i] = Integer.parseInt(a);
-            }
-
-        }
-        return numsList;
-    }
-
-    public static int[] setComputerNums() {
-        int[] computerNums = new int[3];
-
-        for (int i = 0 ; i < 3 ; i++) {
-            computerNums[i] = pickNumberInRange(1,9);
-        }
-
-        return computerNums;
-    }
-
-
     public static int[] countStrikeBall(int[] user, int[] com) {
         int strike =0;
         int ball=0;
 
-        //스트라이크 찾기
         for (int i =0; i<3; i++) {
             if(user[i] == com[i]) {
                 strike++;
             }
         }
 
-        //볼찾기 (자리는 다른 같은 수 개수)
         for (int i=0; i<3; i++) {
             for (int j=0; j<3; j++) {
                 ball = addBall(user,com,ball,i,j) ;
             }
         }
         return new int[] {strike,ball};
-    }
-
-    public static int addBall(int[] user, int[] com, int ball, int i ,int j ) {
-        if ((i != j) && (user[i] == com[j])) {
-            ball++;
-        }
-        return ball;
     }
 
     public static boolean printResult(int strike, int ball){
@@ -131,18 +94,6 @@ public class Application {
         }
     }
 
-    public static void checkdiffNums(String k) {
-        int[] nums = new int[10];
-        for (int i = 0 ; i< 3; i++) {
-            int a = Integer.parseInt(String.valueOf(k.charAt(i)));
-            if (nums[a] == 0) {
-                nums[a] = a;
-            } else {
-                throw new IllegalArgumentException();
-            }
-
-        }
-    }
     public static boolean wantGameRestart() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String restart = readLine();
@@ -163,4 +114,64 @@ public class Application {
             return false;
         }
     }
+
+    public static void checkdiffNums(String k) {
+        int[] nums = new int[10];
+        for (int i = 0 ; i< 3; i++) {
+            int a = Integer.parseInt(String.valueOf(k.charAt(i)));
+            if (nums[a] == 0) {
+                nums[a] = a;
+            } else {
+                throw new IllegalArgumentException();
+            }
+
+        }
+    }
+
+    public static int[] detachNum(String k) {
+        int[] numsList = new int[3];
+        for (int i = 0; i < 3; i++) {
+            char b = k.charAt(i);
+            String a = String.valueOf(b);
+            if (checkInputNum(a, 1, 9)) {
+                numsList[i] = Integer.parseInt(a);
+            }
+
+        }
+        return numsList;
+    }
+
+    public static int addBall(int[] user, int[] com, int ball, int i ,int j ) {
+        if ((i != j) && (user[i] == com[j])) {
+            ball++;
+        }
+        return ball;
+    }
+
+    public static boolean checkInputNum(String a,int start,int end) throws IllegalArgumentException{
+
+        try {
+            int b = Integer.parseInt(a) ;
+            if ((start <= b) && (b <=end)) {
+                return true;
+            } else {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e){
+            throw e;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
