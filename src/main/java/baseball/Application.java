8bd6a98aa9022f5f343fd2f6352e45263ebb4efe
CHAEGODA <chae_goda@naver.com>
2024-02-22 07:53:26 +0900

    refactor : Application11
     answerNum() 함수명 createNum()으로 변경 /
     배열을 리스트로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,9 @@
 package baseball;
+import java.util.*;
 import mallang.missionutils.Randoms;
 import mallang.missionutils.Console;
+
+import static mallang.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
@@ -9,11 +12,11 @@ public class Application {
     }
     // 게임 시작
     public static void startGame() {
-        int[] answer = answerNum();
+        List<Integer> answerNum = answerNum();
 
         while(true) {
-            int[] guess = userNum();
-            int[] result = checkNum(answer, guess);
+            List<Integer> userNum = new ArrayList<>();
+            List<Integer> result = checkNum(answer, userNum);
             printResult(result);
 
             if (result[2] == 3) {
@@ -25,11 +28,10 @@ public class Application {
             }
         }
     }
-    // 1부터 9까지의 서로 다른 임의의 수를 3개 생성해서 배열에 저장 (숫자가 겹치지 않도록 수정)
-    public static int[] answerNum() {
-        int[] answer = new int[3];
-        for(int i = 0; i < answer.length; i++) {
-            answer[i] = Randoms.pickNumberInRange(1,9);
+    public static ArrayList<Integer> createNum() {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(int i = 0; i < answer.size(); i++) {
+            answer.set(i, Randoms.pickNumberInRange(1, 9));
         }
         return answer;
     }
@@ -37,12 +39,12 @@ public class Application {
     // 사용자가 잘못된 값 입력할 경우 IllegalArgumentException 발생 (만들어야 함)
     public static int[] userNum() {
         System.out.println("숫자를 입력해주세요 : ");
-        String input = Console.readLine();
-        int[] guess = parseInput(input);
+        String input = readLine();
+        List<Integer> guess = parseInput(input);
         return guess;
     }
     // 입력받은 String값 Int로 파싱해주기
-    public static int[] parseInput(String input) {
+    public static List<Integer> parseInput(String input) {
         int[] guess = new int[3];
         for(int i = 0; i < guess.length; i++) {
             guess[i] = Character.getNumericValue(input.charAt(i));
@@ -50,7 +52,7 @@ public class Application {
         return guess;
     }
     // 정답 확인 함수
-    public static int[] checkNum(int[] answer, int[] guess) {
+    public static List<Integer> checkNum(List<Integer> answer, List<Integer> guess) {
         int[] result = new int[3];
         countBalls(answer, guess, result);
         countStrikes(answer, guess, result);
@@ -58,11 +60,11 @@ public class Application {
         return result;
     }
     // 볼 개수
-    public static void countBalls(int[] answer, int[] guess, int[] result) {
-        for(int i = 0; i < answer.length; i++) {
-            for(int j = 0; j < guess.length; j++) {
-                if(answer[i] == guess[j] && i != j) {
-                    result[0]++;
+    public static void countBalls(List<Integer> answer, List<Integer> guess, List<Integer> result) {
+        for(int i = 0; i < answer.size(); i++) {
+            for(int j = 0; j < guess.size(); j++) {
+                if(answer.get(i) == guess.get(j) && i != j) {
+                    result.get(0)++;
                 }
             }
         }
@@ -82,8 +84,11 @@ public class Application {
     // 게임 재시작 여부
     public static boolean continueGame() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        int choice = Integer.parseInt(Console.readLine());
-        return choice == 1;
-    }
+        String restart = readLine();
 
+        if (restart.length() != 1) {
+            throw new IllegalArgumentException();
+        }
+
+    }
 }
