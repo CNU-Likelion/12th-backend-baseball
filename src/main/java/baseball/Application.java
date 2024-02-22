4601e7eaacb5fc73875fc70caa5436e7e1b0c764
CHAEGODA <chae_goda@naver.com>
2024-02-22 17:58:09 +0900

    refactor : Application15
    게임 재시작 함수 continueGame() 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,7 +12,7 @@ public class Application {
     }
     // 게임 시작
     public static void startGame() {
-        List<Integer> answerNum = answerNum();
+        List<Integer> answerNum = createNum();
 
         while(true) {
             List<Integer> userNum = new ArrayList<>();
@@ -28,15 +28,15 @@ public class Application {
             }
         }
     }
-    public static ArrayList<Integer> createNum() { // 변경 O
-        ArrayList<Integer> answer = new ArrayList<>();
+    public static List<Integer> createNum() { // 변경 O
+        ArrayList<Integer> answer = new List<>();
         for(int i = 0; i < answer.size(); i++) {
             answer.set(i, Randoms.pickNumberInRange(1, 9));
         }
         return answer;
     }
 
-    public static ArrayList<Integer> userNum() {
+    public static List<Integer> userNum() { // 변경 O
         String input;
         System.out.println("숫자를 입력해주세요 : ");
 
@@ -45,12 +45,12 @@ public class Application {
             throw new IllegalArgumentException();
         }
         alreadyNum(input);
-        ArrayList<Integer> guess = parseInput(input);
+        List<Integer> guess = parseInput(input);
         return guess;
     }
     public static void alreadyNum(String input) { // 변경 O
         boolean containNum;
-        ArrayList<Integer> nums = new ArrayList<>();
+        List<Integer> nums = new ArrayList<>();
         for(int i = 0; i < 3; i++) {
             int idx = Integer.parseInt(String.valueOf(input.charAt(i)));
 
@@ -63,8 +63,8 @@ public class Application {
             }
         }
     }
-    public static ArrayList<Integer> parseInput(String input) {
-        ArrayList<Integer> guess = new ArrayList<>();
+    public static List<Integer> parseInput(String input) { // 변경 O
+        List<Integer> guess = new List<>();
         for(int i = 0; i < 3; i++) {
             char num = input.charAt(i);
             String strNum = String.valueOf(num);
@@ -74,44 +74,67 @@ public class Application {
         }
         return guess;
     }
-    // 정답 확인 함수
-    public static List<Integer> checkNum(List<Integer> answer, List<Integer> guess) {
-        int[] result = new int[3];
-        countBalls(answer, guess, result);
-        countStrikes(answer, guess, result);
-        result[2] = 3 - result[0] - result[1]; // 낫싱 개수
-        return result;
-    }
-    // 볼 개수
-    public static void countBalls(List<Integer> answer, List<Integer> guess, List<Integer> result) {
-        for(int i = 0; i < answer.size(); i++) {
-            for(int j = 0; j < guess.size(); j++) {
-                if(answer.get(i) == guess.get(j) && i != j) {
-                    result.get(0)++;
+    public static boolean checkInput(String input, int start, int end) throws IllegalArgumentException { // 오류발생
+        try {
+            int num = Integer.parseInt(input);
+            if((start <= num) && (num <= end)) {
+                return true;
+            }
+            else {
+                throw new IllegalArgumentException();
+            }
+            catch (IllegalArgumentException e) {
+                throw e;
+            }
+        }
+
+        // 정답 확인 함수
+        public static List<Integer> compareNum(List<Integer> answer, List<Integer> guess) {
+            List<Integer> result = new ArrayList<>();
+            countBalls(answer, guess, result);
+            countStrikes(answer, guess, result);
+            result.set(2, 3 - result.get(0) - result.get(1)); // 낫싱 개수
+            return result;
+        }
+        // 볼 개수
+        public static void countBalls(List<Integer> answer, List<Integer> guess, List<Integer> result) {
+            for(int i = 0; i < answer.size(); i++) {
+                for(int j = 0; j < guess.size(); j++) {
+                    if(answer.get(i).equals(guess.get(j)) && i != j) {
+                        result.set(0, result.get(0) + 1);
+                    }
                 }
             }
         }
-    }
-    // 스트라이크 개수
-    public static void countStrikes(int[] answer, int[] guess, int[] result) {
-        for(int i = 0; i < answer.length; i++){
-            if(answer[i] == guess[i]) {
-                result[1]++;
+        // 스트라이크 개수
+        public static void countStrikes(List<Integer> answer, List<Integer> guess, List<Integer> result) {
+            for(int i = 0; i < answer.size(); i++) {
+                if(answer.get(i).equals(guess.get(i))) {
+                    result.set(1, result.get(1) + 1);
+                }
             }
         }
-    }
     // 결과 출력 (출력 수정 필요)
-    public static void printResult(int[] result) {
-        System.out.println(result[0] + "볼" + " " + result[1] + "스트라이크" + result[2] + "낫싱");
+    public static void printResult(List<Integer> result) {
+            System.out.println(result.get(0) + "볼 " + result.get(1) + "스트라이크 " + result.get(2) + "낫싱");
     }
     // 게임 재시작 여부
-    public static boolean continueGame() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String restart = readLine();
+        public static boolean continueGame() {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String restart = readLine();
 
-        if (restart.length() != 1) {
-            throw new IllegalArgumentException();
+            if (restart.length() != 1) {
+                throw new IllegalArgumentException();
+            }
+
+            checkInput(restart,1,2);
+
+            if (restart.equals("1")) {
+                return true;
+            } else if (restart.equals("2")) {
+                return false;
+            } else {
+                return false;
+            }
         }
-
-    }
 }
