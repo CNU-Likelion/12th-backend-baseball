64db03394d91538171ecddda73e07f717103b9ef
minji <qkralswl6125@naver.com>
2024-02-22 22:22:45 +0900

    refactor: 게임 종료 메소드 분리 및 주석 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,38 +8,35 @@ import java.util.*;
 public class Application {
     public static void main(String[] args) {
         while (true) {
-            //게임 실행
             playGame();
 
-            //게임 종료 후 옵션 선택
-            System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: ");
-            String option = Console.readLine();
-            if ("2".equals(option)) {
+            if (!restartGame()) {
                 break;
             }
         }
     }
 
-    /*게임 메소드*/
+    private static boolean restartGame() {
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: ");
+        String option = Console.readLine();
+        return "1".equals(option);
+    }
+
     private static void playGame() {
-        //임의의 3자리 수 생성
         int[] answer = generateRandomNumbers();
         System.out.println("숫자 야구 답 : " + Arrays.toString(answer));
 
         while (true) {
             System.out.print("숫자를 입력해주세요: ");
             String input = Console.readLine();
-            //입력값 유효성 검사
             if (!isValidInput(input)) {
                 throw new IllegalArgumentException("잘못된 입력입니다.");
             }
 
-            //임의의 수와 입력값 비교
             int[] guess = parseInput(input);
             String result = getResult(answer, guess);
             System.out.println(result);
 
-            //게임 종료
             if (result.equals("3스트라이크")) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 break;
@@ -47,7 +44,6 @@ public class Application {
         }
     }
 
-    /*임의의 수 생성하는 메소드*/
     private static int[] generateRandomNumbers() {
         int[] numbers = new int[3];
         Set<Integer> uniqueDigits = new HashSet<>();
@@ -62,7 +58,6 @@ public class Application {
         return numbers;
     }
 
-    /*입력값 유효성 검사 메소드*/
     private static boolean isValidInput(String input) {
         if (input.length() != 3) {
             throw new IllegalArgumentException("3자리 숫자가 아닙니다.");
@@ -75,7 +70,6 @@ public class Application {
         return true;
     }
 
-    /*입력값 파싱하는 메소드*/
     private static int[] parseInput(String input) {
         int[] numbers = new int[3];
         for (int i = 0; i < 3; i++) {
@@ -84,7 +78,6 @@ public class Application {
         return numbers;
     }
 
-    /*값 비교하여 힌트 얻는 메소드*/
     private static String getResult(int[] answer, int[] guess) {
         int strike = 0;
         int ball = 0;
@@ -106,7 +99,6 @@ public class Application {
         }
     }
 
-    /*볼 체크*/
     private static boolean contains(int[] array, int num) {
         for (int i : array) {
             if (i == num) {
